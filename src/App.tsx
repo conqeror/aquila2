16122eb62db97dd97df19a4326f1f99f9335a182
Samuel Cibulka <samuel.cibulka@projectdrgn.com>
2020-05-17 15:28:56 +0200

    Add rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import "./App.css";
 import { Tabs, Tab, AppBar } from "@material-ui/core";
 import { Home } from "./components/Home/Home";
 import { Rules } from "./components/Rules/Rules";
@@ -14,17 +13,17 @@ const App: React.FC = () => (
       path="/"
       render={({ location }) => (
         <>
-          <AppBar position="static">
+          <AppBar position="sticky">
             <Tabs value={location.pathname}>
               <Tab label="Domov" value="/" component={Link} to={allTabs[0]} />
               <Tab
-                label="Pravidla"
+                label="Pravidlá"
                 value="/rules"
                 component={Link}
                 to={allTabs[1]}
               />
               <Tab
-                label="Prihlasene timy"
+                label="Prihlasené tímy"
                 value="/teams"
                 component={Link}
                 to={allTabs[2]}
--- src/components/Rules/Rules.tsx
+++ src/components/Rules/Rules.tsx
@@ -1,3 +1,135 @@
 import React from "react";
+import { Container } from "@material-ui/core";
 
-export const Rules: React.FC = () => <div>Rules</div>;
+export const Rules: React.FC = () => (
+  <Container>
+    <h1>Pravidlá</h1>
+    <h3>Základy</h3>
+    <p>
+      Aquila je šifrovacia hra. Aj keď asi viete, o čo ide, v tomto roku je
+      situácia okolo šifrovačiek špecifická. Preto si tieto pravidlá pozorne
+      prečítajte.
+    </p>
+    <p>
+      Aquila sa uskutoční 8. 8. 2020, teda skôr, ako sme pôvodne plánovali.
+      Začne o deviatej ráno a bude trvať 10 hodín. Hra sa ale neuskutoční na
+      jednom centrálnom mieste - chceme sa vyhnúť väčšej koncentrácii tímov.
+      Každý tím bude mať svoju vlastnú trasu. Nepôjde o online hru, ale o
+      ozajstnú hru v teréne.
+    </p>
+    <p>
+      Princípom hry je lúštiť šifry. Vylúštením šifry získate heslo, pomocou
+      ktorého zistíte, kam pokračovať ďalej. Na danom mieste si odomknete ďalšiu
+      šifru. Cieľom je absolvovať celú trasu pred vypršaním časového limitu.
+    </p>
+    <h3>Tímy</h3>
+    <p>
+      Počet hrajúcich hráčov v tíme je maximálne 4. Títo hráči musia byť
+      plnoletí. Hru možno poňať aj ako výlet s deťmi, ktoré sa nepočítajú do
+      kapacity tímu - za deti ale zodpovedajú rodičia v plnom rozsahu.
+    </p>
+    <h3>Registrácia</h3>
+    <p>
+      Registrovať sa môžete na tejto stránke. Chceli by sme vás požiadať o
+      férovosť a ak budete vedieť, že na hru nakoniec nepôjdete, odhláste sa
+      prosím.
+    </p>
+    <p>
+      Pri registrácii okrem mena tímu a členov tímu zadáte aj mesto, kde by ste
+      chceli hru hrať a adresu, na ktorú vám poštou doručíme obálku s hernými
+      šiframi.
+    </p>
+    <p>
+      Keď sa už do hry registrujete, budeme očakávať, že budete tieto pravidlá
+      poznať a dodržiavať ich.
+    </p>
+    <h3>Systém hry</h3>
+    <p>
+      Hrou vás bude sprevádzať herná aplikácia, ktorá bude sledovať vašu polohu
+      a s ktorou budete komunikovať. Aplikáciu zverejníme pred hrou.
+    </p>
+    <p>
+      Pred hrou pre vás v meste, ktoré ste si zvolili, vygenerujeme unikátnu
+      trasu (približne 10 stanovíšť), ktorú však nebudete poznať. Budete vedieť
+      len polohu, z ktorej odštartujete o 9:00.
+    </p>
+    <p>
+      Keď sa dostavíte na štart, aplikácia to pomocou GPS rozpozná a oznámi vám,
+      ktorú šifru máte lúštiť. Riešením šifry je heslo, ktoré zadáte do
+      aplikácie. Ak bude správne, aplikácia vám oznámi ďalšie miesto na trase.
+    </p>
+    <p>
+      Hneď ako sa na toto ďalšie miesto dostavíte, vám aplikácia oznámi, ktorú
+      šifru máte lúštiť ako ďalšiu. Postup opakujete až do prejdenia trasy alebo
+      do vypršania časového limitu hry.
+    </p>
+    <p>Je zjavné, že hromadný štart ani cieľ v tomto roku nebude.</p>
+    <p>
+      Organizátori budú hru sledovať online a v prípade technických problémov
+      ochotní pomôcť.
+    </p>
+    <p>
+      Očakávame fair play, aj keď sa na vás nebude nikto pozerať. Nelúštite
+      šifry predtým, než vám to aplikácia povie. Nepoužívajte aplikácie na
+      modifikáciu GPS. Taktiež prosím nespolupracujte medzi tímami. Nezapájajte
+      do riešenia šifier cudzie osoby (nevyužívajte ich ani na zisťovanie
+      informácií encyklopedickej povahy). Nerobte prehrešky proti dobrým mravom
+      a už vôbec nie proti zákonom, vyhláškam, či nariadeniam vlády. Ak si nie
+      ste istí, či je vaše konanie v rozpore s pravidlami, tak pravdepodobne je
+      a nerobte to. Je nám jasné, že v tomto formáte sa hra dá okašľať
+      desiatkami rôznych spôsobov. Buďte preto prosím rozumní.
+    </p>
+    <p>
+      Po trase sa môžete presúvať len pešo. Špeciálne to znamená zákaz MHD,
+      bicyklov, kolobežiek, áut a podobne.
+    </p>
+    <h3>Šifry</h3>
+    <p>
+      Šifry k vám dodáme ešte pred hrou poštou. Obálka bude obsahovať tak šifry,
+      ktoré do hry patria, ako aj falošné šifry, ktoré počas hry neotvoríte.
+      Pravdepodobne bude tiež možné šifry pred hrou vyzdvihnúť osobne v
+      Bratislave.
+    </p>
+    <p>
+      Hra bude koncipovaná tak, aby šifry nešli riešiť výhradne online. Je totiž
+      väčšia zábava sedieť nad papierom ako nad telefónom.
+    </p>
+    <p>
+      Organizátorom volajte, ak ste si istí, že ste na správnom mieste, ale
+      aplikácia vám odmieta prezradiť, ktorú šifru lúštiť.
+    </p>
+    <p>
+      Ak je niečo napísané kurzívou, nie je v tom šifra. Ale môže Vám to pri
+      lúštení pomôcť. Alebo aj nie.
+    </p>
+    <h3>Poradie</h3>
+    <p>
+      Aj keď sa budeme snažiť trasy spraviť tak podobné, ako to len pôjde
+      (podobné dĺžky presunov, prevýšenia a podobne), objektivitu zaručiť
+      nevieme. Preto finálne poradie o 19:00 bude určené podľa týchto kritérií:
+    </p>
+    <ol>
+      <li>Najvyššie dosiahnuté stanovište (bez ohľadu na nápovedy)</li>
+      <li>Menší počet nápovied na vylúštených šifrách</li>
+      <li>
+        Menší celkový súčet časov lúštenia šifier (čas od odomknutia stanovišťa
+        po odoslanie správneho riešenia)
+      </li>
+    </ol>
+    <p>Čas na strávený na presune sa teda do poradia nepočíta.</p>
+    <h3>Mimoriadnosti</h3>
+    <p>
+      V prípade, že sa stane niečo nepredvídané alebo na to organizátori naozaj
+      majú dobrý dôvod, môžu nejaké pravidlo dočasne zrušiť, pridať, či upraviť.
+      Takéto pravidlá vždy dostanete v nezašifrovanej podobe.
+    </p>
+    <p>
+      Vzhľadom na situáciu vo svete si vyhradzujeme právo hru zrušiť kedykoľvek
+      pred jej začiatkom, ale aj počas nej.
+    </p>
+    <p>
+      Z veľmi dobrých dôvodov je možné dohodnúť sa na výnimkách z niektorých
+      pravidiel (napríklad ak máte v tíme vozíčkara).
+    </p>
+  </Container>
+);
